ae92a9aa503d267fac34fad1272850ea2db9e832
deathend110 <2735514990@qq.com>
2026-08-02 21:09:42 +0800

    通过二分查找

--- 704.py
+++ 704.py
@@ -4,20 +4,19 @@
 class Solution:
     def search(self, nums: list[int], target: int) -> int:
         left = 0
-        right  = len(nums)
-        mid = int(right / 2)
+        right = len(nums)-1
+        mid = left + int((right-left) / 2)
 
-        while(1):
+        while(left <= right):
+            mid = left + int((right-left) / 2)
             if(nums[mid] == target):
                 return mid
-            elif(nums[mid] < target and left != mid):
-                left = mid
-                mid = int((left + right) / 2)
-            elif(nums[mid] > target and left != mid):
-                right = mid
-                mid = int((left + right) / 2)
-            elif(left == right or left == mid or mid == right):
-                return - 1
+            elif(nums[mid] > target):
+                right = mid - 1
+            elif(nums[mid] < target):
+                left = mid + 1
+
+        return -1
 
 
 if __name__ == "__main__":
