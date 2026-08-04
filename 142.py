bb27c98bb9865df63572b2c22d72211246b5632e
deathend110 <2735514990@qq.com>
2026-08-05 01:30:21 +0800

    142 数学小巧思双指针

--- 142.py
+++ 142.py
@@ -132,25 +132,44 @@ class Solution:
     # 给定一个链表的头节点  head ，返回链表开始入环的第一个节点。 如果链表无环，则返回 null。
     # index 0开
     def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        # 暴力解答 用一个字典存储，key = id，value = node指针。若发现重复id则返回对应的value
-        NodesDict: dict[int, ListNode] = {}
+        # 考虑双指针
+        slow: ListNode = head
+        fast: ListNode = head
+        # 思路很数学。先让fast走2步，slow走一步。若有环，则slow和fast一定环内相遇。找到环内相遇点
+        # 先找环内相遇点
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
 
-        p: ListNode = head
-        while(p != None):
-            if( id(p) in NodesDict):
-                # p地址已存在，直接返回pid对应的节点
-                return NodesDict[id(p)]
-            # 不存在，保存这个节点并更新
-            NodesDict[id(p)] = p
-            p = p.next
-        
-        return None
+            if slow == fast:
+                break
+        else:
+            return None
+        # 结束循环,fast == slow == 相遇点
+        meet = fast
+        # 然后是slow和fast在环内相遇时一定是slow进环的第一圈。因为slow进环时fast一定在环内。
+        # 而Vfast速度是Vslow的两倍。所以从slow进环开始算，fast走了slow两倍路程。
+        # 设slow在环入口时fast走了环内k步,环长n点.则两者相遇时,fast走了k+n,slow走了(k+n)/,slow一定没有走过n步.最多刚刚好走n步
+        # 设head到环入口x点,环入口到fs相遇处y点,环剩下的长度z点,环长y+z点.
+        # 则slow走了x+y点, fast走了x+y+n(y+z)点, fast比slow多转了几圈
+        # 又Vfast = 2 * Vslow,所以Dfast = 2 * Dslow, 就是fast路程也是slow两倍
+        #       x+y+n(y+z) = 2(x+y)
+        # 约去多余项,得,我们求的是x:
+        #       x = (n-1)(y+z) + z
+        # 也就是说,x的长度等于n-1圈环长+相遇点到环入口剩下的部分.
+        # 我们从head和相遇点各自设一个指针,让他们同时出发,两者相遇处即为环入口
+        p1: ListNode = head
+        p2: ListNode= meet
+        while(p1 != p2):
+            p1 = p1.next
+            p2 = p2.next
+        return p1
 
 
 if __name__ == "__main__":
     intersectVal = 8
-    nums = [3,2,0,-4]
-    pos = -1
+    nums = [1, 3]
+    pos = 1
 
     # intersectVal = 1
     # listA = [1, 1,2,3]
